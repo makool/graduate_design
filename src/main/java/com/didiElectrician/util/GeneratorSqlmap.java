8244dbae1b0aa6c1a37c189fb092eef3ce993358
makool <452880294@qq.com>
2018-04-02 20:18:33 +0800

    update permission

--- src/main/java/com/didiElectrician/util/GeneratorSqlmap.java
+++ src/main/java/com/didiElectrician/util/GeneratorSqlmap.java
@@ -18,31 +18,31 @@ import java.util.List;
  */
 public class GeneratorSqlmap {
 
-    public void generator() throws Exception{
-
-
-        List<String> warnings = new ArrayList<>();
-        boolean overwrite = true;
-        //加载配置文件
-        File configFile = new File("generatorConfig.xml");
-        ConfigurationParser cp = new ConfigurationParser(warnings);
-        Configuration config = cp.parseConfiguration(configFile);
-        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
-        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
-
-        		callback, warnings);
-        myBatisGenerator.generate(null);
-
-
-    }
-    public static void main(String[] args) throws Exception {
-        try {
-            GeneratorSqlmap generatorSqlmap = new GeneratorSqlmap();
-            generatorSqlmap.generator();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }
+//    public void generator() throws Exception{
+//
+//
+//        List<String> warnings = new ArrayList<>();
+//        boolean overwrite = true;
+//        //加载配置文件
+//        File configFile = new File("generatorConfig.xml");
+//        ConfigurationParser cp = new ConfigurationParser(warnings);
+//        Configuration config = cp.parseConfiguration(configFile);
+//        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+//        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
+//
+//        		callback, warnings);
+//        myBatisGenerator.generate(null);
+//
+//
+//    }
+//    public static void main(String[] args) throws Exception {
+//        try {
+//            GeneratorSqlmap generatorSqlmap = new GeneratorSqlmap();
+//            generatorSqlmap.generator();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
     
 }
